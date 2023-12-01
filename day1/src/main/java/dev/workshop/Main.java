f089fba707bf2e5eddd63d6c98ec9866343d0da8
treyvon <Eugenewhitaker2022@gmail.com>
2023-12-01 17:30:44 -0500

    final

--- day1/src/main/java/dev/workshop/Main.java
+++ day1/src/main/java/dev/workshop/Main.java
@@ -10,12 +10,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("Usage: day1 <path-to-input-file>");
+            System.exit(-1);
         }
 
         String input = Main.readInput(args[0]);
         int result = Main.solution(input.split("\n"));
 
-        System.out.printf("Result: %d", result);
+        System.out.printf("Result: %d%n", result);
     }
 
     private static String readInput(String path) throws IOException {
@@ -23,30 +24,138 @@ public class Main {
     }
 
     private static int solution(String[] splitInput) {
-        int[] calibrationsValues = new int[splitInput.length];
+        return Arrays.stream(splitInput).parallel().map(Main::mapLetterRepresentationToDigitRepresentation).mapToInt(Main::mapCalibrationValueToInt).sum();
+    }
 
-        for (int i = 0; i < splitInput.length; i++) {
-            String input = splitInput[i];
-            StringBuilder value = new StringBuilder();
-            for (int j = 0; j < input.length(); j++) {
-                char c = input.charAt(j);
-                if (Character.isDigit(c)) {
-                    value.append(c);
-                    break;
-                }
-            }
+    private static int mapCalibrationValueToInt(String input) {
+        int[] potential = input.chars().filter(Character::isDigit).toArray();
+        return (potential.length >= 1) ? Integer.parseInt("" + (char) potential[0] + (char) potential[potential.length - 1]) : 0;
+    }
 
-            for (int j = input.length() - 1; j >= 0; j--) {
-                char c = input.charAt(j);
-                if (Character.isDigit(c)) {
-                    value.append(c);
-                    break;
-                }
-            }
+    private static String mapLetterRepresentationToDigitRepresentation(String input) {
+        return Main.mapFistLetterRepresentation(input) + Main.mapLastLetterRepresentation(input);
+    }
 
-            calibrationsValues[i] = Integer.parseInt(value.toString());
+    private static String mapFistLetterRepresentation(String input) {
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+        while (i < input.length() && !Main.checkForwardLetterRepresentation(input, i, input.charAt(i), result)) {
+            i++;
         }
 
-        return Arrays.stream(calibrationsValues).sum();
+        return result.toString();
+    }
+
+    private static String mapLastLetterRepresentation(String input) {
+        StringBuilder result = new StringBuilder();
+        String substring = new StringBuilder(input).reverse().toString();
+
+        int i = 0;
+        while (i < substring.length() && !Main.checkBackwardLetterRepresentation(substring, i, substring.charAt(i), result)) {
+            i++;
+        }
+
+        return result.reverse().toString();
+    }
+
+    // don't see a way to reduce the cognitive complexity from 19 to 15
+    private static boolean checkForwardLetterRepresentation(String input, int i, char c, StringBuilder result) {
+        boolean found = false;
+        switch (c) {
+            case 'e' -> found = matchLetterRepresentation(input, i + 1, 4, "ight", '8', 'e', result);
+            case 'f' -> {
+                if (i + 1 < input.length()) {
+                    switch (input.charAt(i + 1)) {
+                        case 'i' -> found = matchLetterRepresentation(input, i + 2, 2, "ve", '5', 'i', result);
+                        case 'o' -> found = matchLetterRepresentation(input, i + 2, 2, "ur", '4', 'o', result);
+                        default -> result.append(c);
+                    }
+                } else {
+                    result.append('f');
+                }
+            }
+            case 'n' -> found = matchLetterRepresentation(input, i + 1, 3, "ine", '9', 'n', result);
+            case 'o' -> found = matchLetterRepresentation(input, i + 1, 2, "ne", '1', 'o', result);
+            case 's' -> {
+                if (i + 1 < input.length()) {
+                    switch (input.charAt(i + 1)) {
+                        case 'e' -> found = matchLetterRepresentation(input, i + 2, 3, "ven", '7', 'e', result);
+                        case 'i' -> found = matchLetterRepresentation(input, i + 2, 1, "x", '6', 'i', result);
+                        default -> result.append(c);
+                    }
+                } else {
+                    result.append('s');
+                }
+            }
+            case 't' -> {
+                if (i + 1 < input.length()) {
+                    switch (input.charAt(i + 1)) {
+                        case 'h' -> found = matchLetterRepresentation(input, i + 2, 3, "ree", '3', 'h', result);
+                        case 'w' -> found = matchLetterRepresentation(input, i + 2, 1, "o", '2', 'w', result);
+                        default -> result.append(c);
+                    }
+                } else {
+                    result.append('t');
+                }
+            }
+            default -> result.append(c);
+        }
+
+        return found;
+    }
+
+    // don't see a way to reduce the cognitive complexity from 17 to 15
+    private static boolean checkBackwardLetterRepresentation(String input, int i, char c, StringBuilder result) {
+        boolean found = false;
+        switch (c) {
+            case 'e' -> {
+                if (i + 1 < input.length()) {
+                    switch (input.charAt(i + 1)) {
+                        case 'e' -> found = matchLetterRepresentation(input, i + 2, 3, "rht", '3', 'e', result);
+                        case 'n' -> {
+                            if (i + 2 < input.length()) {
+                                switch (input.charAt(i + 2)) {
+                                    case 'i' -> found = matchLetterRepresentation(input, i + 3, 1, "n", '9', 'i', result);
+                                    case 'o' -> {
+                                        result.append('1');
+                                        found = true;
+                                    }
+                                    default -> result.append(c);
+                                }
+                            } else {
+                                result.append('n');
+                            }
+                        }
+                        case 'v' -> found = matchLetterRepresentation(input, i + 2, 2, "if", '5', 'v', result);
+                        default -> result.append(c);
+                    }
+                } else {
+                    result.append('e');
+                }
+            }
+            case 'n' -> found = matchLetterRepresentation(input, i + 1, 4, "eves", '7', 'n', result);
+            case 'o' -> found = matchLetterRepresentation(input, i + 1, 2, "wt", '2', 'o', result);
+            case 'r' -> found = matchLetterRepresentation(input, i + 1, 3, "uof", '4', 'r', result);
+            case 't' -> found = matchLetterRepresentation(input, i + 1, 4, "hgie", '8', 't', result);
+            case 'x' -> found = matchLetterRepresentation(input, i + 1, 2, "is", '6', 'x', result);
+            default -> result.append(c);
+        }
+
+        return found;
+    }
+
+    private static boolean matchLetterRepresentation(String input, int start, int length, String match, char isMatch, char isNonMatch, StringBuilder result) {
+        if (matchLetterRepresentation(input, start, length, match)) {
+            result.append(isMatch);
+            return true;
+        } else {
+            result.append(isNonMatch);
+            return false;
+        }
+    }
+
+    private static boolean matchLetterRepresentation(String input, int start, int length, String match) {
+        return start + length <= input.length() && input.substring(start, start + length).equals(match);
     }
 }
